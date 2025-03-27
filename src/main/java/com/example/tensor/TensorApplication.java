cc22bfb3d63359af2fff5cd9c27e20218d496d38
gaegx <danilaohremcuk@gmail.com>
2025-03-28 00:30:16 +0300

    start proj

---
+++ src/main/java/com/example/tensor/TensorApplication.java
@@ -0,0 +1,13 @@
+package com.example.tensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TensorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TensorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tensor/TensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TensorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
